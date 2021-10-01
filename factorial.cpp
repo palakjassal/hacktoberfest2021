a07d8f835910809c78e6237d689a3d119138ebef
MeetVaishnav23 <79635039+MeetVaishnav23@users.noreply.github.com>
2021-10-01 18:59:36 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int i,n,f=1;
+	
+	cout<<"Enter Number :";
+	cin>>n;
+	
+	for(i=1; i<=n; i++)
+	{
+		f = f;
+	}
+	
+	cout<<n<<" Factorial is => "<<f;
+	
+	
+}
